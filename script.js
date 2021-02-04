3d809e13fb1cc1d7e33a5c8568b34b3ff3670912
uzuegbu joseph <josephuzuegbu55@gmail.com>
2021-02-04 09:40:28 +0100

    fetched data from localstorage to display on toDos DOM to prevent lost of data after refreshing browner

--- script.js
+++ script.js
@@ -3,7 +3,30 @@ const submit = document.getElementById('submit');
 const todo_text = document.getElementById('todo_text');
 const todoList_container = document.querySelector('.todos_list');
 let textObjArr = localStorage.getItem('localStorageItem')? JSON.parse(localStorage.getItem('localStorageItem')):[];
-console.log(textObjArr);
+// console.log(textObjArr);
+
+
+if(textObjArr.length > 0){
+
+    textObjArr.forEach(element => {
+        let textObj =element;
+
+        //CREATING THE TODO-LIST ITEMS
+        let toDo_wrapper = document.createElement('div');
+        toDo_wrapper.className='toDoList_wrapper';
+        toDo_wrapper.id= `${textObj.id}`;
+        toDo_wrapper.innerHTML=`
+                <h4>${textObj.text} </h4><span><i class="fas fa-trash" onClick ="deleteItem()"></i></span>
+        `
+        todoList_container.appendChild(toDo_wrapper)
+
+    });
+}
+
+
+
+
+
 // unique id
 var ID = function () {
     return '_' + Math.random().toString(36).substr(2, 9);
@@ -23,7 +46,7 @@ function getTodosList(){
     if(todo_text.value == ""){
         console.log("Field can't be empty");
     }else{
-       let textObj ={
+       let textObj = {
            text:'',
            id:''
        };
@@ -41,7 +64,7 @@ function getTodosList(){
         
                 textObjArr.push(textObj);
                 localStorage.setItem('localStorageItem', JSON.stringify(textObjArr))
-            //   console.log(textObjArr);
+           
    
     }
   
